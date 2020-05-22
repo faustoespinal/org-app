ec2ab05a8aac68230263ca39ad2872b35f2205e7
Pawan Shrivastava <pawanshrivastav555@gmail.com>
2020-05-22 10:51:23 -0500

    Update buildscript.sh

--- scripts/buildscript.sh
+++ scripts/buildscript.sh
@@ -1,3 +1,5 @@
 ##!/bin/bash -xe
 IMAGE_TAG="$(date +%Y-%m-%d.%H.%M.%S).$(echo $CODEBUILD_RESOLVED_SOURCE_VERSION | head -c 8)"  
 echo $IMAGE_TAG
+docker build -t $REPOSITORY_URI:$IMAGE_TAG .
+docker tag $REPOSITORY_URI:$IMAGE_TAG $REPOSITORY_URI:$IMAGE_TAG
