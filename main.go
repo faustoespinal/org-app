fbe5ec19949411b2e742c50d36cc62b2e03d25e9
Fausto J Espinal <faustoespinal@gmail.com>
2020-05-28 08:17:09 -0500

    Update main.go

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World.This is a repo test"}
+		res := &response{Message: "Hello World.This is a repository test"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
