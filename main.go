160fd41e0fba92c56570c54609998fb06e027bb5
Pawan Shrivastava <pawanshrivastav555@gmail.com>
2020-06-04 11:37:03 -0500

    Update main.go

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World.This is an architecture test"}
+		res := &response{Message: "Hello World."}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
